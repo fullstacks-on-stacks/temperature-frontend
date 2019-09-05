19bc2d89467a007c5c6627db381e6be7c7ff709c
DannySuarez <suarezd10@gmail.com>
2019-09-05 09:13:56 -0700

    Displaying data in console

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,12 @@
 import React from 'react';
+import { getLocation, getTemperature } from '../services/temperatureApi';
 
 export default function App() {
+  getLocation()
+    .then(res => console.log(res));
+
+  getTemperature()
+    .then(res => console.log(res));
+    
   return <h1>Hello World</h1>;
 }
-  
